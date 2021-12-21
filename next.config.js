a0d88a22a4bf62d789ce595f74886f252f8be273
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-20 22:58:39 -0800

    feat(next.config): Enable reactStrictMode.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+module.exports = {
+  reactStrictMode: true,
+  ...withPWA({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === "development",
+    },
+  }),
+};
