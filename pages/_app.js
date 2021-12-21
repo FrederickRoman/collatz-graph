5f3c8d19254870c2fa29980f156c49440c6610bd
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-20 22:04:23 -0800

    feat(_app): Add app title.

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={cache}>
       <Head>
-        <title>My page</title>
+        <title>Collatz Graph</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
