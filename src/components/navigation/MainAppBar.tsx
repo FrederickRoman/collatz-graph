10048cc83866d17263f479d999e4ed65507c41af
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-20 23:54:57 -0800

    feat(MainAppBar): Add HomeLink with app logo.

--- src/components/navigation/MainAppBar.tsx
+++ src/components/navigation/MainAppBar.tsx
@@ -1,16 +1,11 @@
 import { Theme } from "@mui/material/styles";
 
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
+import createStyles from "@mui/styles/createStyles";
+import makeStyles from "@mui/styles/makeStyles";
 
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-
-import Typography from "@mui/material/Typography";
+import HomeLink from "./link/HomeLink";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -27,28 +22,17 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 function MainAppBar(): JSX.Element {
- const classes = useStyles();
+  const classes = useStyles();
 
- return (
-   <div className={classes.root}>
-     <AppBar position="static">
-       <Toolbar>
-         <IconButton
-           edge="start"
-           className={classes.menuButton}
-           color="inherit"
-           aria-label="menu"
-           size="large">
-           <MenuIcon />
-         </IconButton>
-         <Typography variant="h6" className={classes.title}>
-           Collatz graph
-         </Typography>
-         <Button color="inherit">About</Button>
-       </Toolbar>
-     </AppBar>
-   </div>
- );
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <HomeLink />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default MainAppBar;
---
+++ src/components/navigation/link/HomeLink.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Typography from "@material-ui/core/Typography";
+import Link from "next/link";
+import IStyles from "@/types/interfaces/IStyles";
+import Box from "@material-ui/core/Box";
+
+const styles: IStyles = {
+  mainBar_header: {
+    flexGrow: 1,
+  },
+  mainBar_text: {
+    color: "white",
+    textDecoration: "none",
+  },
+};
+
+function HomeLink(): JSX.Element {
+  return (
+    <>
+      <Link href="/" passHref>
+        <Box component="a">
+          <Image
+            src="/img/collatzgraphLogo_red.svg"
+            alt="Page logo: a graph that spikes twice before going down"
+            width="30"
+            height="30"
+          />
+        </Box>
+      </Link>
+      <Typography variant="h6" style={styles.mainBar_header}>
+        Collatz Graph
+      </Typography>
+    </>
+  );
+}
+
+export default HomeLink;
